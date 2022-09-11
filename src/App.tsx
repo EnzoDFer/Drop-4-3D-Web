a848c5b290141f40168cf009e813963eb7385e6e
enzodfer <enzodfer@outlook.com>
2022-09-11 13:35:57 -0400

    Create BoardFace class

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+class Board {
+  
+}
 
-function App() {
+//0 first row, 1 second, etc
+type IBoardFace = [IBoardRow,IBoardRow,IBoardRow,IBoardRow];
+type IBoardRow = [IBoardItem,IBoardItem,IBoardItem,IBoardItem];
+type IBoardItem = string|undefined;
+
+class BoardFace {
+  private _boardFace: IBoardFace;
+  public get boardFace(){
+    return this._boardFace;
+  }
+  private set boardFace(newBoardFace: IBoardFace) {
+    this._boardFace = newBoardFace;
+  }
+  public updateRow(rowNum:number,player:string){
+    
+  }
+
+  constructor() {
+    this._boardFace = [
+      Array(4) as IBoardRow,
+      Array(4) as IBoardRow,
+      Array(4) as IBoardRow,
+      Array(4) as IBoardRow,
+    ];
+  }
+
+  private dropIntoRow(myArray:IBoardRow,player: string) {
+    const newArray = myArray.reduce((array,slot:IBoardItem)=>{
+      if (slot!==undefined) array.push(slot);
+      return array;
+    },[])
+  }
+}
+
+
+function App() {  
+  const testBoardFace:BoardFace = new BoardFace();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {testBoardFace.boardFace[4]}
     </div>
   );
 }
