e995384917a85c42e1a3f50881e307dd974fc376
EnzoDFer <enzodfer@outlook.com>
2022-09-12 18:41:42 -0400

    Create and test turn change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { BoardFace } from './App';
+import { BoardFace, Game} from './App';
 
 test('Can update row once', ()=> {
   const testBoardFace = new BoardFace();
@@ -14,4 +14,13 @@ test('Can fill row and not exceed', ()=> {
   testBoardFace.dropIntoRow(testBoardFace.boardFace[0],'p1');
   testBoardFace.dropIntoRow(testBoardFace.boardFace[0],'p1');
   expect(testBoardFace.boardFace[0]).toStrictEqual(['p1','p1','p1','p1']);
+});
+
+test('Player properly changes on call',()=>{
+  const newGame = new Game();
+  expect(newGame.player).toBe('P1');
+  newGame.changeTurns(); // pass turn to p2
+  expect(newGame.player).toBe('P2');
+  newGame.changeTurns(); // and back to p1
+  expect(newGame.player).toBe('P1');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+export class Game {
+  private _player: 'P1' | 'P2';
+  private _board: Board;
+  public get player() {
+    return this._player;
+  }
+  public set player(player:'P1' | 'P2') {
+    this._player = player;
+  }
+  public get board() {
+    return this._board;
+  }
+  public changeTurns(): void {
+    this.player==='P1'?this.player='P2':this.player='P1';
+  }
+  constructor() {
+    this._player = 'P1';
+    this._board = new Board();
+  }
+}
+
 
 class Board {
   private _cube: [BoardFace,BoardFace,BoardFace,BoardFace];
