24137d650707f832d08b75d470b131f52cc4e171
EnzoDFer <enzodfer@outlook.com>
2022-10-02 10:37:00 -0400

    Create full-3d cube using layered faces

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,35 @@ function App() {
 
   return (
     //<GameProvider/>
-    <div>
-      <BoardFaceDisplay/>
-    </div>
+    <Container
+      //cube Container
+      sx={{
+        position:'relative',
+        height:`${boardLength}px`,
+        width:`${boardLength}px`,
+      }}
+    >
+      <BoardFaceDisplay
+        sxProp={{
+          transform:`translateZ(${0*cubeSideLength}px)`,
+        }}
+      />
+      <BoardFaceDisplay
+        sxProp={{
+          transform:`translateZ(${1*cubeSideLength}px)`,
+        }}
+      />
+      <BoardFaceDisplay
+        sxProp={{
+          transform:`translateZ(${2*cubeSideLength}px)`,
+        }}
+      />
+      <BoardFaceDisplay
+        sxProp={{
+          transform:`translateZ(${3*cubeSideLength}px)`,
+        }}
+      />
+    </Container>
   );
 }
 
--- src/Components/BoardFaceDisplay.tsx
+++ src/Components/BoardFaceDisplay.tsx
@@ -1,10 +1,18 @@
-import { Container, SimpleGrid } from "@mantine/core";
-import { IBoardFace, IBoardCol } from "../Global";
+import { Container, SimpleGrid, Sx } from "@mantine/core";
+import { IBoardFace, IBoardCol, boardLength } from "../Global";
 import { BoardColDisplay } from "./BoardColDisplay";
 
-export const BoardFaceDisplay = () => {
+export const BoardFaceDisplay = ({sxProp}:{sxProp?:Sx | (Sx | undefined)[] | undefined}) => {
   return (
-    <Container>
+    <Container
+      sx={{
+        position:'absolute',
+        padding: '0',
+        height:`${boardLength}px`,
+        width:`${boardLength}px`,
+        ...sxProp,
+      }}
+    >
       <BoardColDisplay />
       <BoardColDisplay />
       <BoardColDisplay />
